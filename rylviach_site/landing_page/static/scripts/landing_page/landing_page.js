bc7ebc22d6b1ad79d2d199dd254ce5ca445d6a6d
Zach Sommers <zsommers@kickbackpoints.com>
2016-03-14 16:10:13 -0700

    Updated content - probably done for now

---
+++ rylviach_site/landing_page/static/scripts/landing_page/landing_page.js
@@ -0,0 +1,10 @@
+/*
+ * Javascript for the main landing page content
+ */
+
+// Create the flexslider
+$(document).ready(function() {
+    $('.flexslider').flexslider({
+        directionNav: false
+    });
+});
